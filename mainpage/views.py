9b191789851a8956fc87b2be30bea2d0a40df37b
StandGive <SeoCoding123@gmail.com>
2023-12-06 10:24:03 +0900

    Add : 가입 신청서에 작성 내용 -> Model을 통해 postgresql에 저장

--- mainpage/views.py
+++ mainpage/views.py
@@ -1,5 +1,26 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .models import join
+from django.contrib import messages
+from django.http import JsonResponse
 
 def index(request):
     return render(request, '/Users/seojun/vscode/ssf_django/mainpage/templates/main/index.html')
+
+def register_submit(request):
+
+    joins = join()
+
+    name = request.GET['name']
+    studentID = request.GET['email']
+    phoneNumber = request.GET['phone']
+    message = request.GET['message']
+    
+    joins.applications_name = name
+    joins.apllications_studentID = studentID
+    joins.applications_phoneNumber = phoneNumber
+    joins.applications_text = message
+
+    print("저장 성공!")
+    joins.save()
+
+    return redirect('/')
