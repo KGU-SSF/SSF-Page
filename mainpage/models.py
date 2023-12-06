6a0c9285ae4ae47e87bb030ad67823e5164e52ed
StandGive <SeoCoding123@gmail.com>
2023-12-06 10:23:12 +0900

    Add : 가입신청서 Form 모델 설계

--- mainpage/models.py
+++ mainpage/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.core.validators import RegexValidator
 
 # Create your models here.
+class join(models.Model):
+    applications_name = models.CharField(max_length=10)
+    apllications_studentID = models.IntegerField(default=0)
+    applications_phoneNumberRegex = RegexValidator(regex = r'^01([0|1|6|7|8|9]?)-?([0-9]{3,4})-?([0-9]{4})$')
+    applications_phoneNumber = models.CharField(validators=[applications_phoneNumberRegex], max_length=11, unique=True)
+    applications_text = models.TextField()
+
+    #객체 이름 = 사용자 이름
+    def __str__(self):
+        return self.applications_name
